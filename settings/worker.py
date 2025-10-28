d5125571b029dc1f6a8588dfd1f53ec18baa9838
Ajay Kanki <222144676+ajaykanki@users.noreply.github.com>
2025-10-28 16:03:20 +0530

    feat: add worker config validation

--- settings/worker.py
+++ settings/worker.py
@@ -17,11 +17,24 @@ class WorkerSettings(BaseSettings):
     import_paths: list[str] = ["tasks"]
     api_key: str | None = None
 
+    def validate_config(self) -> bool:
+        if not self.api_key:
+            logger.error(
+                "WORKER_API_KEY is not set in environment variables or keyring."
+            )
+            return False
+
+        return True
+
     def model_post_init(self, context):
         if self.api_key is None:
             logger.warning(
                 "WORKER_API_KEY is not set in environment variables. Attempting to retrieve from keyring."
             )
             self.api_key = get_keyring_password("WORKER_API_KEY")
+
+            if not self.validate_config():
+                logger.error("Invalid configuration. Exiting.")
+                exit(1)
 
         return super().model_post_init(context)
