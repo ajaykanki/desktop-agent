906d5281356799cbeb5d34587b13274ced43dfb9
Ajay Kanki <222144676+ajaykanki@users.noreply.github.com>
2025-11-03 13:20:35 +0530

    feat: add support to call internal functions and set attributes to native elements

--- src/desktop_agent/worker/tasks/sap/sap_mappings.py
+++ src/desktop_agent/worker/tasks/sap/sap_mappings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -8,38 +8,61 @@ class ElementType(str, Enum):
     COMBOBOX = "COMBOBOX"
     TABLE = "TABLE"
     CHECKBOX = "CHECKBOX"
-    RADIOBUTTON = "RADIOBUTTON"
-    TREE = "TREE"
+    RADIOBUTTON = "RADFIOBUTTON"
+    SHELL = "SHELL"
 
 
 class ActionType(str, Enum):
     CLICK = "CLICK"
-    PRESS_ENTER = "PRESS_ENTER"
+    ENTER = "ENTER"
     BACK = "BACK"
 
 
-class Action(BaseModel):
+@dataclass
+class Action:
     type: ActionType
     target_id: str | None = None  # GuiElement ID for CLICK action
     description: str | None = None
 
 
-class GuiElement(BaseModel):
+@dataclass
+class CallInternalFunction:
+    func: str
+    params: list[str] | None
+    description: str | None = None
+
+
+@dataclass
+class SetInternalAttribute:
+    attribute: str
+    value: str | None = None
+    description: str | None = None
+
+
+@dataclass
+class GuiElement:
     id: str
     name: str
     type: ElementType = ElementType.TEXT
+    set_attributes: list[SetInternalAttribute] | None = None
+    call_functions: list[CallInternalFunction] | None = None
 
 
-class Screen(BaseModel):
+@dataclass
+class Screen:
     name: str
     elements: list[GuiElement]
     entry_point: list[Action] | None = (
         None  # Actions to perform to reach this screen. This is like pre_actions
     )
+    press_enter: bool = True  # Whether to press enter after filling all fields
+
+
+@dataclass
+class ScreenOrder:
+    name: str
     post_actions: list[Action] | None = None
 
-
-PRESS_ENTER_ACTION = Action(type=ActionType.PRESS_ENTER)
 
 PRESS_HEADER_BUTTON_ACTION = Action(
     type=ActionType.CLICK,
--- src/desktop_agent/worker/tasks/sap/va01_mappings.py
+++ src/desktop_agent/worker/tasks/sap/va01_mappings.py
@@ -3,9 +3,123 @@ from desktop_agent.worker.tasks.sap.sap_mappings import (
     GuiElement,
     Action,
     ElementType,
+    CallInternalFunction,
     ActionType,
-    PRESS_ENTER_ACTION,
     PRESS_HEADER_BUTTON_ACTION,
+)
+
+# Create Sales Order VA01_SCREEN transaction screen:
+VA01_INITIAL = Screen(
+    name="VA01_INITIAL",
+    elements=[
+        GuiElement(name="order type", id=r"wnd[0]/usr/ctxtVBAK-AUART"),
+        GuiElement(
+            name="sales organization",
+            id=r"wnd[0]/usr/ctxtVBAK-VKORG",
+        ),
+        GuiElement(
+            name="distribution channel",
+            id=r"wnd[0]/usr/ctxtVBAK-VTWEG",
+        ),
+        GuiElement(name="division", id=r"wnd[0]/usr/ctxtVBAK-SPART"),
+    ],
+)
+
+VA01_OVERVIEW = Screen(
+    name="VA01_OVERVIEW",
+    elements=[
+        GuiElement(
+            name="sold to party",
+            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/subPART-SUB:SAPMV45A:4701/ctxtKUAGV-KUNNR",
+        ),
+        GuiElement(
+            name="ship to party",
+            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/subPART-SUB:SAPMV45A:4701/ctxtKUWEV-KUNNR",
+        ),
+        GuiElement(
+            name="po number",
+            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/txtVBKD-BSTKD",
+        ),
+        GuiElement(
+            name="po date",
+            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/ctxtVBKD-BSTDK",
+        ),
+    ],
+)
+
+VA01_SALES = Screen(
+    name="VA01_SALES",
+    entry_point=[
+        Action(
+            type=ActionType.CLICK,
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01",
+        ),
+    ],
+    elements=[
+        GuiElement(
+            name="payment terms",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/ctxtVBKD-ZTERM",
+        ),
+        GuiElement(
+            name="incoterms 1",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/lblVBKD-INCO1",
+        ),
+        GuiElement(
+            name="incoterms 2",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/txtVBKD-INCO2",
+        ),
+    ],
+)
+
+VA01_ITEM_OVERVIEW = Screen(
+    name="VA01_ITEM_OVERVIEW",
+    press_enter=False,
+    entry_point=[
+        Action(
+            type=ActionType.CLICK,
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02",
+        ),
+    ],
+    elements=[
+        GuiElement(
+            name="table",
+            type=ElementType.TABLE,
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02/ssubSUBSCREEN_BODY:SAPMV45A:4401/subSUBSCREEN_TC:SAPMV45A:4900/tblSAPMV45ATCTRL_U_ERF_AUFTRAG",
+        ),
+        GuiElement(
+            name="select all",
+            type=ElementType.BUTTON,
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02/ssubSUBSCREEN_BODY:SAPMV45A:4401/subSUBSCREEN_TC:SAPMV45A:4900/subSUBSCREEN_BUTTONS:SAPMV45A:4050/btnBT_MKAL",
+        ),
+    ],
+)
+
+VA01_FAST_DATA_ENTRY = Screen(
+    name="VA01_FAST_DATA_ENTRY",
+    press_enter=False,
+    entry_point=[
+        Action(
+            type=ActionType.CLICK,
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08",
+        ),
+    ],
+    elements=[
+        GuiElement(
+            name="char. display",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/cmbRV45A-MUEBS",
+            type=ElementType.COMBOBOX,
+        ),
+        GuiElement(
+            name="table",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/subSUBSCREEN_TC:SAPMV45A:7905/tblSAPMV45ATCTRL_U_MILL_SE_KONFIG",
+            type=ElementType.TABLE,
+        ),
+        GuiElement(
+            name="select all",
+            type=ElementType.BUTTON,
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/subSUBSCREEN_TC:SAPMV45A:7905/subSUBSCREEN_BUTTONS:SAPMV45A:4050/btnBT_MKAL",
+        ),
+    ],
 )
 
 
@@ -24,7 +138,6 @@ HEADER_SALES = Screen(
             id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4301/ctxtVBAK-WAERK",
         ),
     ],
-    post_actions=[PRESS_ENTER_ACTION],
 )
 
 HEADER_PARTNERS = Screen(
@@ -33,40 +146,53 @@ HEADER_PARTNERS = Screen(
         PRESS_HEADER_BUTTON_ACTION,
         Action(
             type=ActionType.CLICK,
-            target_id=r"usr/tabsTAXI_TABSTRIP_HEAD/tabpT\08",
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\08",
         ),
     ],
     elements=[
         GuiElement(
-            name="table",
-            type=ElementType.TABLE,
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:4352/subSUBSCREEN_PARTNER_OVERVIEW:SAPLV09C:1000/tblSAPLV09CGV_TC_PARTNER_OVERVIEW",
-        )
+            name="partner function",
+            type=ElementType.COMBOBOX,
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:4352/subSUBSCREEN_PARTNER_OVERVIEW:SAPLV09C:1000/tblSAPLV09CGV_TC_PARTNER_OVERVIEW/cmbGVS_TC_DATA-REC-PARVW[0,4]",
+        ),
+        GuiElement(
+            name="partner",
+            type=ElementType.TEXT,
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:4352/subSUBSCREEN_PARTNER_OVERVIEW:SAPLV09C:1000/tblSAPLV09CGV_TC_PARTNER_OVERVIEW/ctxtGVS_TC_DATA-REC-PARTNER[1,4]",
+        ),
     ],
-    post_actions=[PRESS_ENTER_ACTION],
 )
 
 HEADER_TEXTS = Screen(
     name="HEADER_TEXTS",
+    press_enter=False,
     entry_point=[
         PRESS_HEADER_BUTTON_ACTION,
         Action(
             type=ActionType.CLICK,
-            target_id=r"usr/tabsTAXI_TABSTRIP_HEAD/tabpT\09",
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\09",
         ),
     ],
     elements=[
         GuiElement(
-            name="texts_tree",
-            type=ElementType.TREE,
+            name="click_notify_remiter",
+            type=ElementType.SHELL,
             id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\09/ssubSUBSCREEN_BODY:SAPMV45A:4152/subSUBSCREEN_TEXT:SAPLV70T:2100/cntlSPLITTER_CONTAINER/shellcont/shellcont/shell/shellcont[0]/shell",
+            call_functions=[
+                CallInternalFunction(func="selectItem", params=["Z041", "Column1"]),
+                CallInternalFunction(
+                    func="ensureVisibleHorizontalItem", params=["Z041", "Column1"]
+                ),
+                CallInternalFunction(
+                    func="doubleClickItem", params=["Z041", "Column1"]
+                ),
+            ],
         ),  # Select Notify/Remitter here in Column1,
         GuiElement(
-            name="text",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\09/ssubSUBSCREEN_BODY:SAPMV45A:4152/subSUBSCREEN_TEXT:SAPLV70T:2100/cntlSPLITTER_CONTAINER/shellcont/shellcont/shell/shellcont[1]/shell",  # Set the Notify Text here
+            name="notify",
+            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\09/ssubSUBSCREEN_BODY:SAPMV45A:4152/subSUBSCREEN_TEXT:SAPLV70T:2100/cntlSPLITTER_CONTAINER/shellcont/shellcont/shell/shellcont[1]/shell",  # Set the commission here
         ),
     ],
-    post_actions=[PRESS_ENTER_ACTION],
 )
 
 HEADER_ADD_DATA_A = Screen(
@@ -75,7 +201,7 @@ HEADER_ADD_DATA_A = Screen(
         PRESS_HEADER_BUTTON_ACTION,
         Action(
             type=ActionType.CLICK,
-            target_id=r"usr/tabsTAXI_TABSTRIP_HEAD/tabpT\12",
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\12",
         ),
     ],
     elements=[
@@ -93,7 +219,6 @@ HEADER_ADD_DATA_A = Screen(
             id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\12/ssubSUBSCREEN_BODY:SAPMV45A:4309/cmbVBAK-ZORD_TYPE",
         ),
     ],
-    post_actions=[PRESS_ENTER_ACTION],
 )
 
 HEADER_ADD_DATA_B = Screen(
@@ -102,7 +227,7 @@ HEADER_ADD_DATA_B = Screen(
         PRESS_HEADER_BUTTON_ACTION,
         Action(
             type=ActionType.CLICK,
-            target_id=r"usr/tabsTAXI_TABSTRIP_HEAD/tabpT\13",
+            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\13",
         ),
     ],
     elements=[
@@ -141,127 +266,6 @@ HEADER_ADD_DATA_B = Screen(
         GuiElement(
             name="stake holder",
             id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_HEAD/tabpT\13/ssubSUBSCREEN_BODY:SAPMV45A:4312/sub8309:SAPMV45A:8309/ctxtVBAK-ZZSTAKE_HOLDER",
-        ),
-    ],
-    post_actions=[PRESS_ENTER_ACTION],
-)
-
-VA01_INITIAL = Screen(
-    name="VA01_INITIAL",
-    elements=[
-        GuiElement(name="order type", id=r"wnd[0]/usr/ctxtVBAK-AUART"),
-        GuiElement(
-            name="sales organization",
-            id=r"wnd[0]/usr/ctxtVBAK-VKORG",
-        ),
-        GuiElement(
-            name="distribution channel",
-            id=r"wnd[0]/usr/ctxtVBAK-VTWEG",
-        ),
-        GuiElement(name="division", id=r"wnd[0]/usr/ctxtVBAK-SPART"),
-    ],
-    post_actions=[
-        PRESS_ENTER_ACTION,  # Press enter to continue to Create Sales Order - Overview Screen
-    ],
-)
-
-VA01_OVERVIEW = Screen(
-    name="VA01_OVERVIEW",
-    elements=[
-        GuiElement(
-            name="sold to party",
-            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/subPART-SUB:SAPMV45A:4701/ctxtKUAGV-KUNNR",
-        ),
-        GuiElement(
-            name="ship to party",
-            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/subPART-SUB:SAPMV45A:4701/ctxtKUWEV-KUNNR",
-        ),
-        GuiElement(
-            name="po number",
-            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/txtVBKD-BSTKD",
-        ),
-        GuiElement(
-            name="po date",
-            id=r"wnd[0]/usr/subSUBSCREEN_HEADER:SAPMV45A:4021/ctxtVBKD-BSTDK",
-        ),
-    ],
-    post_actions=[
-        PRESS_ENTER_ACTION,  # Press enter to fetch delivery details
-    ],
-)
-
-VA01_SALES = Screen(
-    name="VA01_SALES",
-    entry_point=[
-        Action(
-            type=ActionType.CLICK,
-            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01",
-        ),
-    ],
-    elements=[
-        GuiElement(
-            name="payment terms",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/ctxtVBKD-ZTERM",
-        ),
-        GuiElement(
-            name="incoterms 1",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/lblVBKD-INCO1",
-        ),
-        GuiElement(
-            name="incoterms 2",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\01/ssubSUBSCREEN_BODY:SAPMV45A:4400/ssubHEADER_FRAME:SAPMV45A:4440/txtVBKD-INCO2",
-        ),
-    ],
-    post_actions=[
-        PRESS_ENTER_ACTION,  # Press enter to redetermine freight
-    ],
-)
-
-VA01_ITEM_OVERVIEW = Screen(
-    name="VA01_ITEM_OVERVIEW",
-    entry_point=[
-        Action(
-            type=ActionType.CLICK,
-            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02",
-        ),
-    ],
-    elements=[
-        GuiElement(
-            name="table",
-            type=ElementType.TABLE,
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02/ssubSUBSCREEN_BODY:SAPMV45A:4401/subSUBSCREEN_TC:SAPMV45A:4900/tblSAPMV45ATCTRL_U_ERF_AUFTRAG",
-        ),
-        GuiElement(
-            name="select all",
-            type=ElementType.BUTTON,
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\02/ssubSUBSCREEN_BODY:SAPMV45A:4401/subSUBSCREEN_TC:SAPMV45A:4900/subSUBSCREEN_BUTTONS:SAPMV45A:4050/btnBT_MKAL",
-        ),
-    ],
-)
-
-VA01_FAST_DATA_ENTRY = Screen(
-    name="VA01_FAST_DATA_ENTRY",
-    entry_point=[
-        Action(
-            type=ActionType.CLICK,
-            target_id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08",
-        ),
-    ],
-    elements=[
-        GuiElement(
-            name="char. display",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/cmbRV45A-MUEBS",
-            type=ElementType.COMBOBOX,
-        ),
-        GuiElement(
-            name="table",
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/subSUBSCREEN_TC:SAPMV45A:7905/tblSAPMV45ATCTRL_U_MILL_SE_KONFIG",
-            type=ElementType.TABLE,
-        ),
-        GuiElement(
-            name="select all",
-            type=ElementType.BUTTON,
-            id=r"wnd[0]/usr/tabsTAXI_TABSTRIP_OVERVIEW/tabpT\08/ssubSUBSCREEN_BODY:SAPMV45A:7901/subSUBSCREEN_TC:SAPMV45A:7905/subSUBSCREEN_BUTTONS:SAPMV45A:4050/btnBT_MKAL",
         ),
     ],
 )
