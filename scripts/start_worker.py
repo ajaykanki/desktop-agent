f167cf3ab682d2b0509cf8108723056d4b534570
Ajay Kanki <222144676+ajaykanki@users.noreply.github.com>
2025-11-16 15:01:54 +0530

    refactor(worker): create validate_configs and apply_schema function to clean main run_worker method

--- scripts/start_worker.py
+++ scripts/start_worker.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from app.logging import log
 from app.worker.core import app
 from app.config import config
@@ -9,13 +8,24 @@ logging.basicConfig(level=logging.DEBUG if config.is_dev else logging.WARNING)
 
 def run_worker():
     log.info("Starting worker...")
+    validate_configs()
+    apply_schema()
+    app.run_worker(
+        concurrency=config.worker.concurrency,
+        name=config.worker.name,
+        queues=config.worker.queues,
+    )
 
+
+def apply_schema():
     with app.open():
         try:
             app.schema_manager.apply_schema()
         except Exception:
             log.warning("Schema already applied or failed to apply schema")
 
+
+def validate_configs():
     if not config.worker.validate_config():
         log.error("Worker configuration is invalid. Exiting.")
         exit(1)
@@ -23,12 +33,6 @@ def run_worker():
     if not config.sap.validate_config():
         log.error("SAP configuration is invalid. Exiting.")
         exit(1)
-
-    app.run_worker(
-        concurrency=config.worker.concurrency,
-        name=config.worker.name,
-        queues=config.worker.queues,
-    )
 
 
 if __name__ == "__main__":
